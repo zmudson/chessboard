f1b2f1aaee2e50f6bd3c6ec0658644b286ad8fcb
Dominik Zmuda-Trzebiatowski <dominik.zmuda_trzebiatowski@wp.pl>
2021-08-27 16:24:53 +0200

    refactor code, add MoveHandler, handle Rook move

--- src/chessboard/pieces/Rook.java
+++ src/chessboard/pieces/Rook.java
@@ -1,5 +1,8 @@
 package chessboard.pieces;
 
+import chessboard.ChessboardGenerator;
+import chessboard.Main;
+import utils.MoveHandler;
 import utils.Position;
 
 import java.util.ArrayList;
@@ -15,8 +18,51 @@ public class Rook extends Piece {
         super(row, column, name, filename, power, color);
     }
 
+    // add move to possible moves array and return true if next position is available or false otherwise
+    private boolean getPossibleMove(int row, int column, List<Position> possibleMoves, Piece piece){
+        boolean isNext = false;
+        if(MoveHandler.isValid(this, piece)){
+            possibleMoves.add(new Position(row, column));
+            if(piece == null)
+                isNext = true;
+        }
+        return isNext;
+    }
+
     public List<Position> getPossibleMoves(Vector<Piece> pieces){
         /* TODO */
-        return new ArrayList<>();
+        // castling
+
+        List<Position> possibleMoves = new ArrayList<>();
+
+        // get possible moves from current position to left border
+        for(int column = this.column - 1; column >= 0; column--){
+            Piece piece = ChessboardGenerator.getPiece(this.row, column, pieces);
+            if(!getPossibleMove(this.row, column, possibleMoves, piece))
+                break;
+        }
+
+        // get possible moves from current position to right border
+        for(int column = this.column + 1; column <= Main.columns - 1; column++){
+            Piece piece = ChessboardGenerator.getPiece(this.row, column, pieces);
+            if(!getPossibleMove(this.row, column, possibleMoves, piece))
+                break;
+        }
+
+        // get possible moves from current position to top border
+        for(int row = this.row - 1; row >= 0; row--){
+            Piece piece = ChessboardGenerator.getPiece(row, this.column, pieces);
+            if(!getPossibleMove(row, this.column, possibleMoves, piece))
+                break;
+        }
+
+        // get possible moves from current position to bottom border
+        for(int row = this.row + 1; row <= Main.rows - 1; row++){
+            Piece piece = ChessboardGenerator.getPiece(row, this.column, pieces);
+            if(!getPossibleMove(row, this.column, possibleMoves, piece))
+                break;
+        }
+
+        return possibleMoves;
     }
 }
