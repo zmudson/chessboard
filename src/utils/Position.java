0b2a439166f6f6660f9b3048de2b0b3c7ecc3703
Dominik Zmuda-Trzebiatowski <dominik.zmuda_trzebiatowski@wp.pl>
2021-08-27 12:16:54 +0200

    refactor Position.java

--- src/utils/Position.java
+++ src/utils/Position.java
@@ -4,11 +4,27 @@ package utils;
 
 public class Position {
 
-    public int row;
-    public int column;
+    private int row;
+    private int column;
 
     public Position(int row, int column) {
         this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
         this.column = column;
     }
 }
